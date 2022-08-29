226a1648fbeb317931abab094fd7cffcbb1e57fa
Yusupov Fayzullo <111003996+YusupovFayzullo@users.noreply.github.com>
2022-08-29 16:38:06 +0500

    Update PrintingFalse.java

--- src/main/java/lang/print/gaps/finalModuleTask/PrintingFalse.java
+++ src/main/java/lang/print/gaps/finalModuleTask/PrintingFalse.java
@@ -4,6 +4,7 @@ public class PrintingFalse {
     public static void main(String[] args) {
         int first = 9;
         int second = -17;
-        //write code here
+        boolean b=(-17>9);
+        System.out.println(b);
     }
 }
