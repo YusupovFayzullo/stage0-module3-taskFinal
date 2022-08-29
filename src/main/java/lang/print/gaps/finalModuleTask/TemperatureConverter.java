354172ab648fbb73a09e02f9b7a53bef0c797d15
Yusupov Fayzullo <111003996+YusupovFayzullo@users.noreply.github.com>
2022-08-29 16:43:25 +0500

    Update TemperatureConverter.java

--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -2,5 +2,8 @@ package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
+        
+        float f=(9*temperatureCelsius)/5+32;
+        System.out.println(f);
     }
 }
